b6517f549c0aab3aaeec1511c9b5f3b729b8af64
Irina22-04 <baranovskaya.irina.87@mail.ru>
2021-05-19 17:38:10 +0300

    fit: added theme for material-ui

--- src/theme.js
+++ src/theme.js
@@ -3,27 +3,37 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
-    fontFamily: 'Rambla, sans-serif',
+    fontFamily: 'Inter, sans-serif',
+    subtitle1: {
+      fontSize: 34,
+    },
+    body1: {
+      fontSize: 21,
+    },
+    caption: {
+      fontSize: 16,
+    },
   },
   palette: {
     primary: {
-      main: '#283e4a',
+      main: '#ED30AD',
       contrastText: '#fff',
     },
     secondary: {
-      main: '#bfc5c9',
+      main: '#A6A6A6',
+      dark: '#646464',
     },
     error: {
-      main: '#f44336',
+      main: '#F43939',
     },
     success: {
       main: '#4caf50',
     },
     background: {
-      default: '#fff',
+      default: '#E5E5E5',
     },
     link: {
-      main: '#317a7a',
+      main: '#0C5FA5',
     },
   },
 });
